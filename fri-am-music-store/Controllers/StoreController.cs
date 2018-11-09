2180c16020a85f2d5de40a583735037df3e7d20c
ifotn <rich@infrontofthenet.com>
2018-11-09 09:14:43 -0500

    lesson 9 part 1. cart

--- fri-am-music-store/Controllers/StoreController.cs
+++ fri-am-music-store/Controllers/StoreController.cs
@@ -38,5 +38,72 @@ namespace fri_am_music_store.Controllers
             ViewBag.Product = ProductName;
             return View();
         }
+
+        // GET: Store/AddToCart
+        public ActionResult AddToCart(int AlbumId)
+        {
+            // determine CartId to group this user's items together
+            string CurrentCartId;
+            GetCartId();
+            CurrentCartId = Session["CartId"].ToString();
+
+            // create and save a new cart item
+            Cart cart = new Cart
+            {
+                CartId = CurrentCartId,
+                AlbumId = AlbumId,
+                Count = 1,
+                DateCreated = DateTime.Now
+            };
+
+            db.Carts.Add(cart);
+            db.SaveChanges();
+
+            // load the shopping cart page
+            return RedirectToAction("ShoppingCart");
+        }
+
+        private void GetCartId()
+        {
+            // if there is no Cart Id session variable (this is their first item)
+            if (Session["CartId"] == null)
+            {
+                // is the user logged in?
+                if (User.Identity.IsAuthenticated)
+                {
+                    Session["CartId"] = User.Identity.Name;
+                }
+                else
+                {
+                    // anonymous so generate new unique string
+                    Session["CartId"] = Guid.NewGuid().ToString();
+                }
+            }
+        }
+
+        // GET: Store/ShoppingCart
+        public ActionResult ShoppingCart()
+        {
+            // check or generate Session CartId
+            GetCartId();
+
+            // select all the items in the current user's cart
+            string CurrentCartId = Session["CartId"].ToString();
+            var CartItems = db.Carts.Where(c => c.CartId == CurrentCartId).ToList();
+
+            return View(CartItems);
+        }
+
+        // GET: Store/RemoveFromCart/5
+        public ActionResult RemoveFromCart(int id)
+        {
+            // find and delete this record from this user's cart
+            Cart CartItem = db.Carts.SingleOrDefault(c => c.RecordId == id);
+            db.Carts.Remove(CartItem);
+            db.SaveChanges();
+
+            // reload the updated cart page
+            return RedirectToAction("ShoppingCart");
+        }
     }
 }
