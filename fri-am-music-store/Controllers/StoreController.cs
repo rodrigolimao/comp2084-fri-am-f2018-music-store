424c5981edbfcb8ee644cdd28e42351e393c1d4a
ifotn <rich@infrontofthenet.com>
2018-11-09 10:47:27 -0500

    lesson 9. checkout

--- fri-am-music-store/Controllers/StoreController.cs
+++ fri-am-music-store/Controllers/StoreController.cs
@@ -47,16 +47,29 @@ namespace fri_am_music_store.Controllers
             GetCartId();
             CurrentCartId = Session["CartId"].ToString();
 
-            // create and save a new cart item
-            Cart cart = new Cart
-            {
-                CartId = CurrentCartId,
-                AlbumId = AlbumId,
-                Count = 1,
-                DateCreated = DateTime.Now
-            };
+            // if item is already in the user's cart, increment count by 1
+            Cart cart = db.Carts.SingleOrDefault(c => c.AlbumId == AlbumId &&
+                c.CartId == CurrentCartId);
 
-            db.Carts.Add(cart);
+            if (cart == null)
+            {
+                // create and save a new cart item
+                cart = new Cart
+                {
+                    CartId = CurrentCartId,
+                    AlbumId = AlbumId,
+                    Count = 1,
+                    DateCreated = DateTime.Now
+                };
+
+                db.Carts.Add(cart);
+            }
+            else
+            {
+                // increment the count by 1
+                cart.Count++;
+            }
+
             db.SaveChanges();
 
             // load the shopping cart page
@@ -104,6 +117,85 @@ namespace fri_am_music_store.Controllers
 
             // reload the updated cart page
             return RedirectToAction("ShoppingCart");
+        }
+
+        // GET: Store/Checkout
+        [Authorize]
+        public ActionResult Checkout()
+        {
+            // migrate the cart to the username if shopping anonymously
+            MigrateCart();
+            return View();
+        }
+
+        // POST: Store/Checkout
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Checkout(FormCollection values)
+        {
+            // create a new order & populate it from the form values
+            Order order = new Order();
+            TryUpdateModel(order);
+
+            // autopopulate the fields that we can
+            order.Username = User.Identity.Name;
+            order.Email = User.Identity.Name;
+            order.OrderDate = DateTime.Now;
+
+            // get user's items and calc cart total
+            var CartItems = db.Carts.Where(c => c.CartId == User.Identity.Name);
+            decimal OrderTotal = (from c in CartItems
+                                  select (int)c.Count * c.Album.Price).Sum();
+            order.Total = OrderTotal;
+
+            // save main order
+            db.Orders.Add(order);
+            db.SaveChanges();
+
+            // now save each item
+            foreach (Cart item in CartItems)
+            {
+                OrderDetail od = new OrderDetail
+                {
+                    OrderId = order.OrderId,
+                    AlbumId = item.AlbumId,
+                    Quantity = item.Count,
+                    UnitPrice = item.Album.Price
+                };
+
+                db.OrderDetails.Add(od);
+            }
+
+            // save all the order items
+            db.SaveChanges();
+
+            // show the confirmation / receipt / invoice page 
+            return RedirectToAction("Details", "Orders", new { id = order.OrderId });
+        }
+
+        private void MigrateCart()
+        {
+            // attach anonymous cart to an authenticated once they log in
+            if (!String.IsNullOrEmpty(Session["CartId"].ToString()) && User.Identity.IsAuthenticated)
+            {
+                if (Session["CartId"].ToString() != User.Identity.Name)
+                {
+                    // get cart items with the random id
+                    string CurrentCartId = Session["CartId"].ToString();
+                    var CartItems = db.Carts.Where(c => c.CartId == CurrentCartId);
+
+                    foreach (Cart item in CartItems)
+                    {
+                        item.CartId = User.Identity.Name;
+                    }
+
+                    db.SaveChanges();
+
+                    // update the session variable to the username
+                    Session["CartId"] = User.Identity.Name;
+                }
+            }
         }
     }
 }
